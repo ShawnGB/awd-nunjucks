6cdf58191bcd4b6b49208ff25cbc8f801b92a3ed
Shawn GB <god.shawn@gmail.com>
2025-11-03 14:40:06 +0100

    feat(nunjucks): integrate nunjucks for templating
    - Add nunjucks and its types to dependencies
    - Configure nunjucks in express
    - Render a base template in the index handler
    - Add .prettierrc for code formatting

--- src/handler/indexhandler.ts
+++ src/handler/indexhandler.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 
 const indexHandler = (req: Request, res: Response) => {
-  res.send("Hello Again");
+  res.setHeader("content-type", "text/html");
+  res.render("base.njk");
 };
 
 export { indexHandler };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import "dotenv/config";
 import express from "express";
 import { indexHandler } from "./handler/indexhandler";
+import nunjucks from "nunjucks";
 
 const app = express();
 
 const port = process.env.PORT || 3030;
 
+nunjucks.configure("src/templates", {
+  autoescape: true,
+  express: app,
+});
+app.set("view engine", "njk");
 app.get("/", indexHandler);
 
 app.listen(port, () => {
